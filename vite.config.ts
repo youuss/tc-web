b71abaaffdc0f98a5b72f92d31e5a42be796c259
youus <alecshen0926@foxmail.com>
2023-02-27 12:13:10 +0800

    🏗️ 架构调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,21 @@
+import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [
+    vue(),
+    WindiCSS({
+      scan: {
+        include: ['src/**/*.{vue,html,jsx,tsx}', 'index.html'],
+      },
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@/': `${path.resolve(__dirname, 'src')}/`,
+    },
+  },
 })
---
+++ windi.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite-plugin-windicss'
+
+export default defineConfig({
+  darkMode: 'class',
+  plugins: [
+  ],
+  attributify: true,
+  theme: {
+    extend: {
+    },
+  },
+})

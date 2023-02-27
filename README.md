1de275e8ee9c6f494daed8f93c6b31e45c562aaf
youus <alecshen0926@foxmail.com>
2023-02-27 11:33:14 +0800

    🎉 project init

---
+++ README.md
@@ -0,0 +1,10 @@
+# TC-Web
+pnpm安装
+```shell
+npm install -g pnpm
+```
+
+项目初始化
+```shell
+pnpm i
+```
---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-3xl font-bold underline">
+    Hello world!
+  </div>
+</template>

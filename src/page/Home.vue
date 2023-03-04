456e9f05f596e17830dd8838a927374e148b21bf
dev_Jin <eryejhn@163.com>
2023-03-04 15:31:16 +0800

    :art: 菜单栏样式尝试

---
+++ src/page/Home.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>(主体内容)</div>
+</template>
+
+<script>
+    export default {
+        name: "Home"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -10,7 +10,20 @@
 import { createRouter, createWebHistory } from "vue-router";
 import type { RouteRecord } from "vue-router";
 
-const routes: RouteRecord[] = [];
+const routes: RouteRecord[] = [
+  {
+    path:'/',
+    name:'main',
+    component: () => import('@/page/Main.vue'),
+    children: [
+      {
+        path:'/',
+        name:'main',
+        component: () => import('@/page/Home.vue'),
+      }
+    ]
+  }
+];
 
 const router = createRouter({
   history: createWebHistory(),

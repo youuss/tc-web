f47dd7ff663bbc48a44b1cc355b8596e2c845833
dev_Jin <eryejhn@163.com>
2023-03-10 16:10:46 +0800

    :art: 改进菜单栏

---
+++ src/components/Header.vue
@@ -0,0 +1,86 @@
+<template>
+<!--  border-black border-2 -->
+  <header class="relative w-full bg-white">
+    <div class="h-18 border-b-2 border-solid border-gray-100 rounded-none mb-0 py-1 px-5 ">
+      <div class="absolute float-left w-36">
+        <img class="h-16" src="@/assets/logo.png"/>
+      </div>
+      <div class="">
+        <ul class="w-full flex flex-row justify-center items-center h-16">
+          <li class=" mx-6 inline-block flex transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110" v-for="m in menu"
+              @mouseenter="openMenu"
+              @mouseleave="closeMenu"
+          >
+            {{ m.title }}
+            <span class="ml-1 w-2" v-if="m.children">
+              <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  class="h-5 w-5">
+                <path
+                    fill-rule="evenodd"
+                    d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z"
+                    clip-rule="evenodd" />
+              </svg>
+            </span>
+          </li>
+
+        </ul>
+      </div>
+
+    </div>
+    <div class="hidden border-black border-2">1</div>
+<!--    <nav class="-z-10  inset-x-0 inset-y-0 h-full overflow-visible bg-white duration-300 w-full px-0 py-3.5">-->
+<!--      <ul class="list-unstyled absolute w-full h-full text-center pt-0 flex-row ">-->
+<!--        <li class="inline-block float-none px-2 m-0 py-1" v-for="m in menu" :key="m.name">-->
+<!--          <a class="relative block outline-none no-underline text-gray-500 ">-->
+<!--            {{m.title}}-->
+<!--          </a>-->
+<!--        </li>-->
+<!--      </ul>-->
+<!--    </nav>-->
+  </header>
+
+</template>
+
+<script>
+export default {
+  name: "Header",
+  data(){
+    return {
+      menu:[
+        {title:'首页',name:'Home',route:'',},
+        {title:'关于思碳',name:'About',route:'',children:[
+            {title:'公司介绍',name:'About',route:''},
+            {title:'新闻与活动',name:'News',route:''},
+            {title:'投资我们',name:'About',route:''},
+            {title:'招贤纳士',name:'About',route:''},
+          ]},
+        {title:'我们的产品',name:'Product',route:'',children:[
+            {title:'公司介绍',name:'About',route:''},
+          ]},
+        {title:'我们的技术',name:'Tech',route:'',children:[
+            {title:'公司介绍',name:'About',route:''},
+          ]},
+        {title:'服务中心',name:'Service',route:'',children:[
+            {title:'公司介绍',name:'About',route:''},
+          ]},
+        {title:'联系我们',name:'Contact',route:''},
+      ]
+    }
+  },
+  methods:{
+    openMenu(){
+     console.log(1)
+    },
+    closeMenu(){
+      console.log(2)
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
--- src/page/Main.vue
+++ src/page/Main.vue
@@ -1,19 +1,6 @@
 <template>
     <div>
-        <div class="h-8 border-black border-2 flex justify-center items-center md:h-16 ">
-            <div v-for="m in menu" :key="m.name" class="md:w-30 md:border-grey md:border-2 md:flex md:justify-center hidden">{{m.name}}</div>
-            <div class="md:hidden flex justify-center">
-                <div>浙江思碳亿芯环保技术有限公司</div>
-                <button class="absolute top-1.5 right-2" @click="openMenu()">
-                    <img src="@/assets/view.png" class="w-4">
-                </button>
-            </div>
-        </div>
-        <div class="absolute top-8.5 right-2 w-2/5 border-grey border-1" :hidden="menuSwitch">
-            <div v-for="m in menu" :key="m.name" class="pl-2 pb-1 pt-1">
-                {{m.name}}
-            </div>
-        </div>
+      <Header/>
         <router-view/>
         <div>页脚</div>
     </div>
@@ -21,9 +8,11 @@
 </template>
 <!-- 每页固定部分(菜单，浮窗，页脚) -->
 <script>
+    import Header from "../components/Header.vue";
     export default {
         name: "Main",
-        data(){
+      components: { Header },
+      data(){
             return {
                 menu:[
                     {name:'首页'},
